c0937d20c10e7e4cfb65d6bc181dbf4b9cdcad28
highwide <hochweit728@gmail.com>
2019-03-10 14:34:55 +0900

    Implement a function to save photo url/data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.imageToData = functions.storage.object().onFinalize((object) => {
+  const bucketName = process.env.bucketName;
+  const filePath = object.name;
+  const db = admin.firestore();
+
+  db.collection('images').add({
+    filePath,
+    downloadUrl: `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${encodeURIComponent(filePath)}?alt=media`,
+  }).then(() => console.log('Done'));
+});
