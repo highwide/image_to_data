ed5044b03ad3b1e0c1e03446031c4773cef46c84
highwide <hochweit728@gmail.com>
2019-03-11 23:58:25 +0900

    Implement essential function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,12 +4,13 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 exports.imageToData = functions.storage.object().onFinalize((object) => {
-  const bucketName = process.env.bucketName;
-  const filePath = object.name;
+  const bucketName = process.env.bucketName || '';
+  const filePath = object.name || '';
   const db = admin.firestore();
+  const fileUrlComponent = encodeURIComponent(filePath);
 
   db.collection('images').add({
     filePath,
-    downloadUrl: `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${encodeURIComponent(filePath)}?alt=media`,
-  }).then(() => console.log('Done'));
+    downloadUrl: `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${fileUrlComponent}?alt=media`,
+  }).then(() => console.log('Done')).catch(err => console.log(err));
 });
